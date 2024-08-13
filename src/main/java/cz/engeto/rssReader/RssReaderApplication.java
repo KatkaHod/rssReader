e6288e1474e8d797578a83855d9dd2e5235010c6
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-08-13 16:56:11 +0200

    Initial commit

---
+++ src/main/java/cz/engeto/rssReader/RssReaderApplication.java
@@ -0,0 +1,13 @@
+package cz.engeto.rssReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/engeto/rssReader/RssReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.engeto.rssReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
